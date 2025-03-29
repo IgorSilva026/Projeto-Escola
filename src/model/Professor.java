57e51b238a7a6334eb3be35f2a6d9576cf67ee90
IgorSilva026 <igorssilva026@gmail.com>
2025-03-29 12:01:09 -0300

    feat: "Professor"

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -1,4 +1,36 @@
 package model;
 
 public class Professor {
+
+    private String nomeProfessor;
+    private String iadeProfessor;
+    private String materiaProfessor;
+
+    public Professor(String nomeProfessor, String iadeProfessor, String materiaProfessor) {
+        this.nomeProfessor = nomeProfessor;
+        this.iadeProfessor = iadeProfessor;
+        this.materiaProfessor = materiaProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getIadeProfessor() {
+        return iadeProfessor;
+    }
+
+    public String getMateriaProfessor() {
+        return materiaProfessor;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nomeProfessor='" + nomeProfessor + '\'' +
+                ", iadeProfessor='" + iadeProfessor + '\'' +
+                ", materiaProfessor='" + materiaProfessor + '\'' +
+                '}';
+    }
+}
 }
