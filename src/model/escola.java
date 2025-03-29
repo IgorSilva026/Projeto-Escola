06b36b5a248e4c7833711b9e01389d8d585cdea0
IgorSilva026 <igorssilva026@gmail.com>
2025-03-29 12:34:43 -0300

    feat:  "escola"

--- src/model/escola.java
+++ src/model/escola.java
@@ -1,4 +1,38 @@
 package model;
 
+import java.util.ArrayList;
+
 public class escola {
+
+    private String nomeEscola;
+    private ArrayList<Professor> professores;
+    private ArrayList<Aluno> alunos;
+
+    public escola(String nome) {
+        this.nomeEscola = nome;
+        this.professores = new ArrayList<>();
+        this.alunos = new ArrayList<>();
+    }
+
+    public void adicionaProfessor(Professor professor) {
+        professores.add(professor);
+    }
+    public void adicionaAluno(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    public void listarProfessores() {
+        System.out.println("Lista de professores");
+        for (Professor professor : professores) {
+            System.out.println(professor);
+        }
+    }
+
+    public void listarAlunos() {
+        System.out.println("Lista de alunos");
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+
 }
