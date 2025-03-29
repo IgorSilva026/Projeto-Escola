635307ce3e10b1772a08c4b69b11586d8a6bf6ab
IgorSilva026 <igorssilva026@gmail.com>
2025-03-29 11:47:49 -0300

    feat "ClassAluno"

---
+++ src/model/Aluno.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Aluno {
+    //alt+insert - construtor
+
+    private String nomeAluno;
+    private int idadeAluno;
+    private String matriculaAluno;
+
+    public Aluno(String nomeAluno, int idadeAluno, String matriculaAluno) {
+        this.nomeAluno = nomeAluno;
+        this.idadeAluno = idadeAluno;
+        this.matriculaAluno = matriculaAluno;
+    }
+
+
+    //alt+insert - getter
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public int getIdadeAluno() {
+        return idadeAluno;
+    }
+
+    public String getMatriculaAluno() {
+        return matriculaAluno;
+
+    }
+
+    //alt+insert - topString
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nomeAluno='" + nomeAluno + '\'' +
+                ", idadeAluno=" + idadeAluno +
+                ", matriculaAluno='" + matriculaAluno + '\'' +
+                '}';
+    }
+}
